89c286422590c3f8df0dd4d2bd78b2dfcc80e301
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-28 15:47:20 +0300

    intial commit

---
+++ src/test/java/com/softserve/marathon/MarathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.marathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarathonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
