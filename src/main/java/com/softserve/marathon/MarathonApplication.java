947d58957058146bf8cda0d92c67ef9d92322a54
king-hacki <king-hacki@king-hackis-MacBook-Pro.local>
2020-08-29 10:17:47 +0300

    set connection with database

--- src/main/java/com/softserve/marathon/MarathonApplication.java
+++ src/main/java/com/softserve/marathon/MarathonApplication.java
@@ -2,8 +2,9 @@ package com.softserve.marathon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class MarathonApplication {
 
     public static void main(String[] args) {
